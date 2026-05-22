d698c762bb201c55c2ec2fc5ab27a923d7e0488b
Pranit Kulkarni <162532222+Pranit-Kulkarni@users.noreply.github.com>
2026-05-22 15:54:56 +0000

    Next Phase changes

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 function openInvite(){
 
-  document.querySelector(".opening").style.display = "none";
+  const opening =
+  document.querySelector(".opening");
 
-  document.getElementById("mainContent").style.display = "block";
+  opening.style.opacity = "0";
+
+  setTimeout(() => {
+
+    opening.style.display = "none";
+
+    document.getElementById("content").style.display =
+    "block";
+
+  },1000);
 }
 
 /* COUNTDOWN */
 
-const weddingDate = new Date("April 26, 2026 09:00:00").getTime();
+const weddingDate =
+new Date("April 26, 2026 09:00:00").getTime();
 
 setInterval(() => {
 
-  const now = new Date().getTime();
+  const now =
+  new Date().getTime();
 
-  const distance = weddingDate - now;
+  const distance =
+  weddingDate - now;
 
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const days =
+  Math.floor(distance / (1000 * 60 * 60 * 24));
 
   document.getElementById("countdown").innerHTML =
-    days + " Days To Go ❤️";
+  days + " Days To Go ❤️";
 
-}, 1000);
+},1000);
